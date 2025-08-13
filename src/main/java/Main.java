5f624256bacd5257c600f762fc0506aedeeb2a32
manasw13 <manaswalia04@gmail.com>
2025-08-13 18:59:40 +0530

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,53 +1,55 @@
 import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.ByteBuffer;
 
 public class Main {
+    public static void main(String[] args) {
+        // You can use print statements as follows for debugging, they'll be visible when running
+        // tests.
+        System.err.println("Logs from your program will appear here!");
 
-  public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible
-    // when running tests.
-    System.err.println("Logs from your program will appear here!");
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        int port = 9092;
+        try {
+            serverSocket = new ServerSocket(port);
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
+            // Wait for connection from client.
+            clientSocket = serverSocket.accept();
 
-    // Uncomment this block to pass the first stage
+            InputStream in = new BufferedInputStream(clientSocket.getInputStream());
 
-    ServerSocket serverSocket = null;
-    Socket clientSocket = null;
-    int port = 9092;
-    try {
-      serverSocket = new ServerSocket(port);
-      // Since the tester restarts your program quite often, setting
-      // SO_REUSEADDR ensures that we don't run into 'Address already in use'
-      // errors
-      serverSocket.setReuseAddress(true);
-      // Wait for connection from client.
-      clientSocket = serverSocket.accept();
+            byte[] messageSizeBytes = in.readNBytes(4);
+            System.out.println(messageSizeBytes);
 
-      BufferedInputStream in =
-          new BufferedInputStream(clientSocket.getInputStream());
-      byte[] messageSizeBytes = in.readNBytes(4);
-      int messageSize = ByteBuffer.wrap(messageSizeBytes).getInt();
+            int messageSize = ByteBuffer.wrap(messageSizeBytes).getInt();
 
-      byte[] apiKey = in.readNBytes(2);
-      byte[] apiVersion = in.readNBytes(2);
-      int correlationId = ByteBuffer.wrap(in.readNBytes(4)).getInt();
+            byte[] apiKey = in.readNBytes(2);
+            byte[] apiVersion = in.readNBytes(2);
+            int correlationId = ByteBuffer.wrap(in.readNBytes(4)).getInt();
 
-      clientSocket.getOutputStream().write(messageSizeBytes);
-      var res = ByteBuffer.allocate(4).putInt(correlationId).array();
-      clientSocket.getOutputStream().write(res);
+            clientSocket.getOutputStream().write(messageSizeBytes);
+            var res = ByteBuffer.allocate(4).putInt(correlationId).array();
 
-    } catch (IOException e) {
-      System.out.println("IOException: " + e.getMessage());
-    } finally {
-      try {
-        if (clientSocket != null) {
-          clientSocket.close();
+            clientSocket.getOutputStream().write(res);
+            clientSocket.getOutputStream().write(new byte[] {0, 35});
+
+            // clientSocket.getOutputStream().write(new byte[] {00, 00, 00, 00, 00, 00, 00, 07} );
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        } finally {
+            try {
+                if (clientSocket != null) {
+                    clientSocket.close();
+                }
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            }
         }
-      } catch (IOException e) {
-        System.out.println("IOException: " + e.getMessage());
-      }
     }
   }
-}
